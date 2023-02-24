e7c749a5edadf429a64942ae51a5133d7b003806
Israrezek <125635149+Israrezek@users.noreply.github.com>
2023-02-24 10:06:40 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/layouts/Header';
+import Landing from './components/layouts/Landing';
+import Footer from './components/layouts/Footer';
+
+import { BrowserRouter} from 'react-router-dom'
+import Routers from './components/routers';
+
+
+function App() {
+  return (
+    <div className="App">
+    <BrowserRouter>
+    <Header></Header>
+    <Routers></Routers>
+    <Footer></Footer>
+    </BrowserRouter>
+    </div> 
+  );
+}
+
+export default App;
---
+++ src/components/layouts/Landing.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class Landing extends Component {
+  render() {
+    return (
+        <><section class="text-center landing">
+        <div class="dark-overlay">
+            <div class=" landing-inner">
+                <h1 class="x-large">Devloper Connector</h1>
+                <h4>Create a developer profile/portfolio, share posts and get help from other developers</h4>
+            <div>
+                <a class="btn btn-primary" href="login.html" target="_blank">login </a>
+               <a class="btn btn-light" href="registration.html" target="_blank">register</a>
+              </div>
+            </div>
+        </div>
+
+
+        </section> </>
+    )
+  }
+}
